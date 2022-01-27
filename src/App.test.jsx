6354544feaf78e49dfa21ad2cfadc76b629d3636
karyssa-dandrea <karyssadandrea@gmail.com>
2022-01-26 16:44:47 -0800

    got my tests passing, keep forgetting to work on a branch im so sorry

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,17 +1,19 @@
+import { screen, render } from '@testing-library/react'
+import { rest } from 'msw'
+import { setupServer } from 'msw/node'
+
 // When in doubt check the docs!
 // 🚨🚨 https://mswjs.io/docs/ 🚨🚨
 
-import { screen, render } from '@testing-library/react'
 // 🚨
-// import rest
-// import setupServer
+
 import App from './App'
 
 const user = {
   id: 1,
   created_at: '2021-12-13T00:17:29+00:00',
   // 🚨 Add a name here
-  name: '',
+  name: 'Vonta',
   avatar: 'https://thumbs.gfycat.com/NiceRequiredGrunion-size_restricted.gif',
   header: 'https://static.wikia.nocookie.net/naruto/images/5/50/Team_Kakashi.png',
   likes: ['React', 'Anime', 'Traveling', 'Living', 'Tower Defense Games', 'Card Games'],
@@ -20,14 +22,23 @@ const user = {
 }
 
 // 🚨 Create your server
+const server = setupServer(
+  rest.get('https://uzgiamkrbapxufnwdrja.supabase.co/rest/v1/users', (req, res, ctx) => {
+    const select = req.url.searchParams.get('select')
+    if (select === '*') {
+      return res(ctx.json([user]))
+    }
+    return res(ctx.status(500), ctx.json({ error: 'failed' }))
+  })
+)
 
 // 🚨 Listen for server start
-beforeAll()
+beforeAll(() => server.listen())
 
 // 🚨 Close server when complete
-afterAll()
+afterAll(() => server.close())
 
-test('Should render the header', async () => {
+test.only('Should render the header', async () => {
   render(<App />)
   const banner = screen.getByRole('banner')
   const headerImg = screen.getByAltText(/alchemy/i)
@@ -53,6 +64,16 @@ test('Should render the header with Sasuke 🌬️🔥', async () => {
   }
 
   // 🚨 Use the server to change the response for this test
+
+  server.use(
+    rest.get('https://uzgiamkrbapxufnwdrja.supabase.co/rest/v1/users', (req, res, ctx) => {
+      const select = req.url.searchParams.get('select')
+      if (select === '*') {
+        return res(ctx.json([sasuke]))
+      }
+      return res(ctx.status(500), ctx.json({ error: 'failed' }))
+    })
+  )
 
   render(<App />)
 
